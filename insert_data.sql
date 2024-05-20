902500732a92b38eb58f048acf331585ddb55624
b-randon707 <58225958+b-randon707@users.noreply.github.com>
2024-05-20 14:11:28 -0500

    Add files via upload

---
+++ insert_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO laredo_for_lease (
+    address, industrial_park, building_size, space_available, minimum_area, office_size, 
+    dock_high_doors, grade_level_doors, trailer_parking, car_parking, clear_height, 
+    fire_protection, nnn_rate_sfpermonth, nnn_expenses_sfpermonth, total_gross_rate, 
+    available_status, year_built, construction_type, dock_configuration, contact_name, contact_number, 
+    contact_email, date_added, date_removed
+)
+VALUES (
+   
+);
+
+
