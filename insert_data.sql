f7316fd99b9162d13bfc182301896ec8ca402f1d
Research Analyst <researchanalyst@Property-Manger-STXCRE-iMac.local>
2024-05-21 17:18:19 -0500

    up_to_13505_RegionalDr

--- insert_data.sql
+++ insert_data.sql
@@ -6,26 +6,25 @@ INSERT INTO laredo_for_lease (
     contact_email, date_added, date_removed
 )
 VALUES (
-    '11909 Hayter Road', 'Eastpoint Industrial Road', 144976, 72488, 72488, 2925,
-    10, 1, 22, 96, 28, 
-    'ESFR', 0.85, 0.16, 1.01, 
-    'October 1, 2024', 2006, 'Concrete tilt', 'Rear loaded', 'Edward Villareal', NULL, 
-    NULL, NULL, NULL
+    '1905 Madison St', 'South of Santa Maria Industrial Park, South', 777666, 40080, 40080, 4302, 
+    9, 1, NULL, NULL, 24,
+    'None', 1.15, NULL, NULL, 
+    'Available', 1985, 'Metal', 'Side loaded', 'Daniel Galvan', '956-631-1322',
+    'dgalvan@cbcworldwid.com', '10/16/23', NULL
+),
 
+(
+    '902 Nicolas D Hachar Rd', 'Pinnacle Industrial Park', 292914, 40000, 40000, NULL,
+     3, NULL, 95, NULL, 40, 
+    'ESFR', NULL, NULL, 0.83,
+    'Available', 2021, 'Concrete tilt', 'Side loaded', 'Gary Bender', '214-206-4942', 
+    'gbender@crausa.com', '10/17/23', NULL
 ), 
 
 (
-    '18722 Metropolitan Rd', 'Millenium Park', 56640, 56640, 56640, NULL, 
-    23, 2, 36, 27, 22,
-    NULL, 0.73, 0.13, 0.86, 
-    'Available', 2010, 'Metal', 'Rear loaded', 'Carlo Molano', '956-523-9403',
-    'carlom@forumcre.com', '7/25/23', NULL
-), 
-
-(
-    '13599 N Lamar Dr., Bldg E', 'Killam Industrial Park', 55525, 55525, 55525, 3730,
-    8, 1, 38, NULL, NULL, 
-    'none', 1.15, NULL, NULL, 
-    'Available', 1993, 'Concrete tilt', 'Rear loaded', 'Tano Carranza', '956-724-7141', 
-    'tcarranza@killamco.com', '4/11/24', NULL
+    '13505 Regional Dr', 'Unitec Industrial Drive', 39422, 39422, 39422, 4297,
+    4, 3, 5, NULL, 24,
+    NULL, NULL, NULL, NULL,
+    'Available', 1992, 'Metal', 'Front loaded', 'Mike Rodriguez', '956-286-1926', 
+    'miker@forumcre.com', '3/12/24', NULL
 ); 
