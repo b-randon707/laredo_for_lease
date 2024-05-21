a18c6482c5946abf18c3368b88cdf5a0c9abd448
Research Analyst <researchanalyst@Property-Manger-STXCRE-iMac.local>
2024-05-21 13:14:15 -0500

    added_to_153599N_lamarDr

--- insert_data.sql
+++ insert_data.sql
@@ -6,11 +6,25 @@ INSERT INTO laredo_for_lease (
     contact_email, date_added, date_removed
 )
 VALUES (
- 
-    '11018 Delta Dr', 'Eastpoint Industrial Park', 120000, 77868, 77868, 7050,
-    17, 2, 55, NULL, 24,
-    NULL, NULL, NULL, NULL, 
-    'Available', 2001, 'Concrete tilt', 'Rear loaded', 'Carlo Molano', '956-523-9403', 
-    'carlom@forumcre', NULL, NULL
+    '11909 Hayter Road', 'Eastpoint Industrial Road', 144976, 72488, 72488, 2925,
+    10, 1, 22, 96, 28, 
+    'ESFR', 0.85, 0.16, 1.01, 
+    'October 1, 2024', 2006, 'Concrete tilt', 'Rear loaded', 'Edward Villareal', NULL, 
+    NULL, NULL, NULL
 
-)
+), 
+
+(
+    '18722 Metropolitan Rd', 'Millenium Park', 56640, 56640, 56640, NULL, 
+    23, 2, 36, 27, 22,
+    NULL, 0.73, 0.13, 0.86, 
+    'Available', 2010, 'Metal', 'Rear loaded', 'Carlo Molano', '956-523-9403',
+    'carlom@forumcre.com', '7/25/23', NULL
+), 
+
+(
+    '13599 N Lamar Dr., Bldg E', 'Killam Industrial Park', 55525, 55525, 55525, 3730,
+    8, 1, 38, NULL, NULL, 
+    'none', 1.15, NULL, NULL, 
+    'Available', 1993, 'Concrete tilt', 'Rear loaded', 'Tano Carranza', '956-724-7141', 
+    'tcarranza@killamco.com', '4/11/24', NULL
